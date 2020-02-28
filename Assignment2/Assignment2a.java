f175f0098c9ebee6339988522bf4a6aee7bbb63e
Josh <joshua.duda@gmail.com>
2020-02-27 21:56:08 -0600

    Completed Assignment 2A - Comments required!!

--- Assignment2/Assignment2a.java
+++ Assignment2/Assignment2a.java
@@ -1,20 +1,28 @@
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Assignment2a {
 
-	public static void permutermIndex(String fileName) throws FileNotFoundException {
-		File f = new File(fileName);
-		Scanner sc = new Scanner(f);
+	public static void permutermIndex(String fileName) throws IOException {
+
+		List<String> stopwords = Files.readAllLines(Paths.get("common_stopwords.txt"));
+
+		BufferedReader br = new BufferedReader(new FileReader(fileName));
+		Scanner file = new Scanner(br);
 
 		ArrayList<String> list = new ArrayList<String>();
-		
-		while (sc.hasNextLine()) {
-			StringTokenizer st = new StringTokenizer(sc.nextLine());
+		ArrayList<String> permInd = new ArrayList<String>();
+
+		while (file.hasNextLine()) {
+			StringTokenizer st = new StringTokenizer(file.nextLine());
 
 			while (st.hasMoreTokens()) {
 
@@ -24,28 +32,49 @@ public class Assignment2a {
 
 				word = word.toLowerCase();
 
-				list.add(word);
-				
+				if (!stopwords.contains(word)) {
+					list.add(word);
+				}
+
 			}
 		}
-		sc.close(); // closing our scanner
-		
+		file.close(); // closing our scanner
+
+		// initial sort
 		Collections.sort(list);
-		
+
+		// removing duplicate words
+		for (int i = 1; i < list.size(); i++) {
+			if (list.get(i).equals(list.get(i - 1))) {
+				list.remove(i - 1);
+				i = i - 1;
+			}
+		}
+
 		for (int i = 0; i < list.size(); i++) {
-			System.out.println(list.get(i));
+			String base = list.get(i) + "$";
+			permInd.add(base + " : " + list.get(i));
+			while (!base.substring(0, 1).equals("$")) {
+				base = base.substring(1, base.length()) + base.substring(0, 1);
+				permInd.add(base + " : " + list.get(i));
+			}
 		}
 		
+		Collections.sort(permInd);
+
+		for(int i = 0; i < permInd.size(); i++) {
+			System.out.println(permInd.get(i));
+		}
 	}
-	
-	public static void main(String [] args) throws FileNotFoundException {
+
+	public static void main(String[] args) throws IOException {
 		String file = "";
 		Scanner input = new Scanner(System.in);
 		System.out.println("Please enter the file you want a permuterm index of: ");
 		file = input.next();
-		
+
 		permutermIndex(file);
-		
+
 	}
 
 }
